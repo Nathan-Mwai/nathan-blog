3f1ed9c47008acd96a45feed6ca83fa9e882e774
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-06-25 17:50:05 +0300

    add: Implement new blog card

---
+++ components/blog/Blog-Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {BlogProps} from "@/components/blog/Blog";
+
+const BlogCard = ({id, title, img,time,category}:BlogProps) => {
+    return (
+        <div>BlogCard</div>
+    )
+}
+export default BlogCard
