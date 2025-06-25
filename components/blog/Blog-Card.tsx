63ef55c4a40c543c1833706d6a6ca334b117347c
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-06-25 18:10:23 +0300

    add: Interate props to the card as well as assign initial styling

--- components/blog/Blog-Card.tsx
+++ components/blog/Blog-Card.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import {BlogProps} from "@/components/blog/Blog";
+import {ArrowRight, ArrowUpRight} from "lucide-react";
 
 const BlogCard = ({id, title, img,time,category}:BlogProps) => {
     return (
-        <div>BlogCard</div>
+        <>
+            <div>
+                <div>
+                    <div>
+                        <img src={img} alt="" />
+                    </div>
+                    <div>
+                        <div className={'flex items-center justify-between'}>
+                            <h1>{title}</h1>
+                            <div className={'bg-slate-200 rounded-sm p-1'}>
+                            <ArrowRight className={'size-4 stroke-2'}/>
+                            {/*<ArrowUpRight/>*/}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 export default BlogCard
