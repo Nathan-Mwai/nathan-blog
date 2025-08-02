b63acf3f8ee7a93f058b58f5b18e6efb16fdf64b
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-08-02 11:51:39 +0300

    final implementation of blog page

--- app/(root)/blog/[id]/page.tsx
+++ app/(root)/blog/[id]/page.tsx
@@ -3,6 +3,10 @@ import {BLOG_BY_ID_QUERY} from "@/sanity/lib/queries";
 import {notFound} from "next/navigation";
 import {formatDate} from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import markdownit from 'markdown-it'
+
+const md = markdownit()
 
 const Page = async ({params}: {params : Promise<{id:string}>}) => {
         const id = (await params).id
@@ -10,6 +14,8 @@ const Page = async ({params}: {params : Promise<{id:string}>}) => {
     const post = await client.fetch(BLOG_BY_ID_QUERY, {id})
 
     if(!post) return notFound()
+
+    const parsedContent = md.render(post?.idea || "")
     return (
         <>
             <section className="section_container !min-h-[230px] flex flex-col lg:flex-row gap-8 lg:gap-12 items-center">
@@ -26,6 +32,27 @@ const Page = async ({params}: {params : Promise<{id:string}>}) => {
                         height={1000} 
                         className="w-full max-w-[500px] h-auto rounded-lg object-cover"
                     />
+                </div>
+            </section>
+
+            <section className="section_container">
+                <div className={'space-y-5 mt-10 max-w-4xl mx-auto'}>
+                    <div className={'flex-between gap-5'}>
+                        <Link href={'https://nathanmwai.com'} target={'_blank'} className={'mb-3'}>
+                            <div>
+                                <p className={'text-16-medium !text-black-300'}>@Nathan Mwai</p>
+                            </div>
+                        </Link>
+                        <p className={'category-tag'}>{post.category}</p>
+                    </div>
+                    {parsedContent ? (
+                        <article
+                            className={"prose max-w-4xl text-lg break-all"}
+                        dangerouslySetInnerHTML={{ __html: parsedContent }}
+                        />
+                    ):(
+                        <p className={"no-result"}>No details provided</p>
+                    )}
                 </div>
             </section>
         </>
